5988c9e7c014cc179155edc5d8528efd7a9fc59d
Yann Hodique <hodiquey@vmware.com>
2015-01-04 04:14:32 -0800

    bump version to 0.3.0

--- marshal.el
+++ marshal.el
@@ -4,7 +4,7 @@
 
 ;; Author: Yann Hodique <hodiquey@vmware.com>
 ;; Keywords: eieio
-;; Version: 0.2.0
+;; Version: 0.3.0
 ;; URL: https://github.com/sigma/marshal.el
 ;; Package-Requires: ((eieio "1.4") (json "1.4)
 
