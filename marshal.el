c4e2fca948c162fc051264e7f5799393c8f93052
Yann Hodique <hodiquey@vmware.com>
2015-09-12 21:57:14 -0700

    version 0.4.2

--- marshal.el
+++ marshal.el
@@ -4,7 +4,7 @@
 
 ;; Author: Yann Hodique <hodiquey@vmware.com>
 ;; Keywords: eieio
-;; Version: 0.4.1
+;; Version: 0.4.2
 ;; URL: https://github.com/sigma/marshal.el
 ;; Package-Requires: ((eieio "1.4") (json "1.3"))
 
